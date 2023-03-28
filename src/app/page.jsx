8cb0e9c869df45c7c5a9da184e7518a82653609b
1109piyush <piyushsinghal11091999@gmail.com>
2023-03-28 21:14:10 +0530

    first commit

---
+++ src/app/page.jsx
@@ -0,0 +1,15 @@
+
+import React from 'react'
+import "../styles/globals.css"
+
+const page = () => {
+  return (
+    <>
+    <h1 className="text-3xl font-bold underline">
+    Hello world!
+  </h1>
+  </>
+  )
+}
+
+export default page
